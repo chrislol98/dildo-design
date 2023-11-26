bfe81cecd200fa27a9033b90abc15cf41c3c3b6b
chris <1729889531@qq.com>
2023-11-27 01:13:25 +0800

    feat: table

--- components/Table/table.tsx
+++ components/Table/table.tsx
@@ -1,19 +1,30 @@
 import * as React from 'react';
 import TBody from './tbody';
-
+import ColGroup from './colgroup'
+import { useComponent } from './shared';
 const Table = function (props, ref) {
+  
   // value
+  const {components} = props;
+  const {ComponentTable, ComponentBodyWrapper, ComponentHeaderWrapper} = useComponent(components);
   const TBodyNode = <TBody data={pageData} columns={flattenColumns} />;
   // const processedData = getProcessedData(currentSorter, activeSorters, innerFilters);
   // const pageData = getPageData();
 
   // xxxxxxxxxxx
 
-  // jsx
+  // render 
   const renderTable = () => {
     return <div></div>;
   };
-  const renderHead = () => {};
+  const renderTHead = () => {
+    return  <ComponentHeaderWrapper >
+    <ComponentTable>
+      <ColGroup columns={flattenColumns}  />
+      {theadNode}
+    </ComponentTable>
+  </ComponentHeaderWrapper>
+  };
   const renderBody = () => {};
   return renderTable();
 };
