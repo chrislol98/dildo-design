8ed581e5803de47b80578345f96a3169237591b1
chris <1729889531@qq.com>
2023-12-04 23:07:36 +0800

    feat: 虚拟里诶博爱

---
+++ components/src/variable-size-list.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { VariableSizeList } from './react-window';
+import './variable-size-list.css';
+
+const rowSizes = new Array(1000).fill(true).map(() => 25 + Math.round(Math.random() * 50));
+
+const getItemSize = (index) => rowSizes[index];
+
+const Row = ({ index, style }) => (
+  <div className={index % 2 ? 'ListItemOdd' : 'ListItemEven'} style={style}>
+    Row {index}
+  </div>
+);
+const App = () => {
+  return (
+    <VariableSizeList
+      className="List"
+      height={200}
+      width={200}
+      itemSize={getItemSize}
+      itemCount={1000}
+    >
+      {Row}
+    </VariableSizeList>
+  );
+};
+export default App;
