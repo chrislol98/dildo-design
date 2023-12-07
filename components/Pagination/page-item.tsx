d07518d3f01bab8d15290941c44b3be75c937563
chris <1729889531@qq.com>
2023-12-08 00:52:18 +0800

    feat(pagination)

--- components/Pagination/page-item.tsx
+++ components/Pagination/page-item.tsx
@@ -1,5 +1,20 @@
-function Item(props) {
-  return <div></div>;
+function Pager(props) {
+  const { pageNum, onClick: _onClick, disabled } = props;
+  const onClick = (e) => {
+    e.stopPropagation();
+    !disabled && onClick?.(pageNum);
+  };
+  return <li>{pageNum}</li>;
 }
-
-export default Item;
+export function JumpPager(props) {
+  const { onClick: _onClick, allPages, current, jumpPage, disabled } = props;
+  // 保证不超过allPages，也不小于minCurrent
+  const minCurrent = allPages > 0 ? 1 : 0;
+  const nextPage = Math.min(allPages, Math.max(minCurrent, current + jumpPage));
+  const onClick = () => {
+    !disabled && _onClick?.(nextPage);
+  };
+  return <li onClick={onClick}>...</li>;
+}
+export function StepPager(props) {}
+export default Pager;
