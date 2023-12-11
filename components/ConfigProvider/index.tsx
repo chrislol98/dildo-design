c93f292737939cb2d45436ced18cae11208020a6
chris <1729889531@qq.com>
2023-12-12 01:51:18 +0800

    feat(configprovider)

--- components/ConfigProvider/index.tsx
+++ components/ConfigProvider/index.tsx
@@ -1,15 +1,62 @@
 import * as React from 'react';
 import defaultLocale from '../locale/default';
-import { useMergeProps, omit } from '../shared';
+import { useMergeProps, omit, isObject } from '../shared';
+import { lighten } from './shared';
 const defaultProps = {
   locale: defaultLocale,
+  prefixCls: 'dildo',
 };
 
+const colorList = {
+  primaryColor: {
+    default: '--arcoblue-6',
+    hover: '--arcoblue-5',
+    active: '--arcoblue-7',
+  },
+  successColor: {
+    default: '--green-6',
+    hover: '--green-5',
+    active: '--green-7',
+  },
+  infoColor: {
+    default: '--arcoblue-6',
+    hover: '--arcoblue-5',
+    active: '--arcoblue-7',
+  },
+  warningColor: {
+    default: '--orangered-6',
+    hover: '--orangered-5',
+    active: '--orangered-7',
+  },
+  dangerColor: {
+    default: '--red-6',
+    hover: '--red-5',
+    active: '--red-7',
+  },
+};
+
+function setTheme(theme) {
+  if (theme && isObject(theme)) {
+    const root = document.body;
+    Object.keys(colorList).forEach((color) => {
+      if (theme[color]) {
+        root.style.setProperty(colorList[color].default, lighten(theme[color], 0));
+
+        root.style.setProperty(colorList[color].hover, lighten(theme[color], 10));
+
+        root.style.setProperty(colorList[color].active, lighten(theme[color], -10));
+      }
+    });
+  }
+}
 export const ConfigContext = React.createContext<any>({ ...defaultProps });
 
 function ConfigProvider(props) {
   props = useMergeProps(props, defaultProps, {});
-  const { locale } = props;
+  const { theme } = props;
+  React.useEffect(() => {
+    setTheme(theme);
+  }, [theme]);
 
   const config = {
     ...omit(props, ['children']),
